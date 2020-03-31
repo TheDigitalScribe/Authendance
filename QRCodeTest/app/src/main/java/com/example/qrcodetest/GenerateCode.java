7776a926c021c9c916c7bf5b26a9640f9350c0eb
Mark <Mark@gmail.com>
2020-03-31 23:53:16 +0100

    added random string generator for qr code generation

--- QRCodeTest/app/src/main/java/com/example/qrcodetest/GenerateCode.java
+++ QRCodeTest/app/src/main/java/com/example/qrcodetest/GenerateCode.java
@@ -5,18 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.Random;
 
 public class GenerateCode extends AppCompatActivity {
 
@@ -40,17 +38,12 @@ public class GenerateCode extends AppCompatActivity {
                 int width = 200;
                 int height = 200;
 
-                private String generateString(int length) {
-                    char[] characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
-                    StringBuilder stringBuilder = new StringBuilder();
-                }
-
-                /*Makes phone keyboard disappear when button is clicked
-                genEditText.onEditorAction(EditorInfo.IME_ACTION_DONE);*/
+                codeField.setText(genRandString());
+                String code = codeField.getText().toString();
 
                 //Creates and displays QR code
                 try {
-                    BitMatrix bitMatrix = qrCodeWriter.encode(textGen, BarcodeFormat.QR_CODE, width, height);
+                    BitMatrix bitMatrix = qrCodeWriter.encode(code, BarcodeFormat.QR_CODE, width, height);
                     Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
 
                     for (int x = 0; x < width; x++) {
@@ -66,5 +59,18 @@ public class GenerateCode extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //Method for generating random string for QR code generation
+    private String genRandString() {
+        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+
+        for(int i = 0; i < 25; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
     }
 }

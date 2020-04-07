35e8f6be0b6f20ccbbe8707fd26df1294786c1ea
Mark <Mark@gmail.com>
2020-04-07 14:34:01 +0100

    updated timer that resets qr code

--- Authendance/app/src/main/java/com/example/authendance/GenerateCode.java
+++ Authendance/app/src/main/java/com/example/authendance/GenerateCode.java
@@ -10,22 +10,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class GenerateCode extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 30000;
 
     private TextView codeField;
     private Button genCodeBtn;
     private ImageView qrCode;
-    private CountDownTimer countDownTimer;
-    private TextView countDownTimerText;
-    private long timeLeftMilliseconds = 30000;
-    private boolean timeRunning;
+
+    private CountDownTimer mCountDownTimer;
+
+    private TextView mTextViewCountDown;
+
+    private long mTimeLeftInMilliseconds = START_TIME_IN_MILLIS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +41,9 @@ public class GenerateCode extends AppCompatActivity {
         codeField = findViewById(R.id.codeField);
         genCodeBtn = findViewById(R.id.genCodeBtn);
         qrCode = findViewById(R.id.qrCode);
-        countDownTimerText = findViewById(R.id.countDownTimerText);
+        mTextViewCountDown = findViewById(R.id.countDownTimerText);
+
+        qrCode.setVisibility(View.INVISIBLE);
 
         genCodeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +54,8 @@ public class GenerateCode extends AppCompatActivity {
 
                 codeField.setText(genRandString());
                 String code = codeField.getText().toString();
+
+                startTimer();
 
                 //Creates and displays QR code
                 try {
@@ -63,58 +73,39 @@ public class GenerateCode extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-                startStop();
             }
         });
     }
 
-    public void startStop() {
-        if(timeRunning) {
-            stopTimer();
-        }
-        else {
-            startTimer();
-        }
-    }
-
-    public void startTimer() {
-        countDownTimer = new CountDownTimer(timeLeftMilliseconds, 1000) {
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMilliseconds, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                timeLeftMilliseconds = millisUntilFinished;
+                qrCode.setVisibility(View.VISIBLE);
+                mTimeLeftInMilliseconds = millisUntilFinished;
                 genCodeBtn.setEnabled(false);
                 updateTimer();
-
             }
 
             @Override
             public void onFinish() {
+                qrCode.setVisibility(View.INVISIBLE);
                 genCodeBtn.setEnabled(true);
+                mTimeLeftInMilliseconds = START_TIME_IN_MILLIS;
+                codeField.setText("");
+                updateTimer();
             }
-        }.start();
-
-        timeRunning = true;
-    }
-
-    public void stopTimer() {
-        countDownTimer.cancel();
-        timeRunning = false;
-    }
-
-    public void updateTimer() {
-        int minutes = (int) timeLeftMilliseconds / 30000;
-        int seconds = (int) timeLeftMilliseconds % 30000 / 1000;
-
-        String timeLeft = "" + minutes;
-        timeLeft += ":";
-
-        if(seconds < 10) {
-            timeLeft += "0";
         }
-        timeLeft += seconds;
+        .start();
+    }
 
-        countDownTimerText.setText(timeLeft);
+
+    private void updateTimer() {
+        int minutes = (int) (mTimeLeftInMilliseconds / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMilliseconds / 1000) % 60;
+
+        String timeLeft = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeft);
     }
 
     //Method for generating random string for QR code generation

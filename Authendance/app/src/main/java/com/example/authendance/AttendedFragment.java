79fcae513cf99703906bccfa1b591cc823c380cf
Mark <Mark@gmail.com>
2020-05-26 16:31:46 +0100

    committed final changes

--- Authendance/app/src/main/java/com/example/authendance/AttendedFragment.java
+++ Authendance/app/src/main/java/com/example/authendance/AttendedFragment.java
@@ -3,23 +3,29 @@
 package com.example.authendance;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -111,7 +117,7 @@ public class AttendedFragment extends Fragment {
             }
         });
 
-        /*Allows the teacher to set the selected student as absent and updates the student's attendance record
+        //Allows the teacher to set the selected student as absent and updates the student's attendance record
         attendAdapter.setOnItemLongClickListener(new AttendanceAdapter.OnItemLongClickListener() {
             @Override
             public void onItemLongClick(DocumentSnapshot documentSnapshot, int position) {
@@ -189,7 +195,7 @@ public class AttendedFragment extends Fragment {
                     Toast.makeText(getActivity(), "Please connect to internet to set attendance", Toast.LENGTH_SHORT).show();
                 }
             }
-        });*/
+        });
     }
 
     public static boolean isConnectedtoInternet(@NonNull Context context) {
